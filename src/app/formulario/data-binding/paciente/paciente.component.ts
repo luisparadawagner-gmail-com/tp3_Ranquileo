acfb8c47a1ecd9cd8ba8777139836cf4cb184580
maxi <maxiranqui@gmail.com>
2020-05-13 08:16:32 -0300

    Uso de Interpolación

--- src/app/formulario/data-binding/paciente/paciente.component.ts
+++ src/app/formulario/data-binding/paciente/paciente.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Paciente} from 'src/app/formulario/data-binding/clases/Paciente';
 @Component({
   selector: 'app-paciente',
   templateUrl: './paciente.component.html',
   styleUrls: ['./paciente.component.css']
 })
 export class PacienteComponent implements OnInit {
+  nombrePaciente: string;
 
-  constructor() { }
+  paciente: Paciente = {
+    nombre: 'Nestor',
+    apellido:'Ortigoza',
+    edad: 33,
+    direccion: 'Boedo 123'
+  };
 
+
+  constructor() {
+    this.nombrePaciente= 'Maxi Ranquileo'
+   }
+  
   ngOnInit(): void {
   }
 
